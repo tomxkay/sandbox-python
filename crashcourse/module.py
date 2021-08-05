0e0ccd742d747e58ef9f8d2229c05805ae740e92
tomxkay <tomxkay@pm.me>
2021-08-04 23:42:36 -0500

    add module.py

---
+++ crashcourse/module.py
@@ -0,0 +1,29 @@
+# storing functions into modules
+
+# storing your functions in a separate file allows you to hide the details of
+# your program's code and focus in a separate file allows you to hide the
+# details of your program's code and focus on its higher-level logic
+
+# code reuse
+# code isolation for sharing
+
+# a module is a file ending in .py
+
+# importing an entire module
+# import module_name
+# module_name.function_name()
+
+# importing specific functions
+# from module_name import function_name
+# from module_name iport function_0, function_1, function_2
+
+# using as to give a function an alias
+# from module_name import function_name as alias
+
+# using as to give a module an alias
+# import module_name as alias
+
+# importing all functions in a module
+# from module_name import *
+
+
